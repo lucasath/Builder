0b3a390f4fe8a7a2321a1af743220d85b6ca2d8c
Lucas Anthony <lucasath@hotmail.com>
2020-06-19 08:57:33 -0300

    feat: Adicionando Estrutura dos componentes

---
+++ src/Components/Talents/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+export default () =>
+    <div className="Talentos">
+        <div className="caixa">
+            <div className="moldura ">
+                <div className="hover">
+                </div>
+            </div>
+            <div className="pontos ativo">
+                <span>0</span>
+                <span>/</span>
+                <span>5</span>
+            </div>
+        </div>
+    </div>
